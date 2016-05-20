88de45c3ba9c1d08952d50b15ec9bd39255626a7
Kitt Hsu <kitt.hsu@gmail.com>
2016-05-20 20:11:15 +0800

    refactored tests

--- test/index.js
+++ test/index.js
@@ -31,32 +31,33 @@ function bz2Codec(dataSize, t) {
   bz2C.end(data);
 }
 
+function fileCodec(inputFile, destinationFile, bz2C, t) {
+  var input = fs.createReadStream(inputFile);
+  const hash = crypto.createHash('md5');
+  const compressedHash = crypto.createHash('md5');
+  compressedHash.update(fs.readFileSync(destinationFile), 'binary');
+
+  var writable = new stream.Writable({
+    write: (chunk, encoding, next) => {
+      hash.update(chunk, 'binary');
+      next()
+    }
+  });
+
+  writable.on('finish', () => {
+    t.eq(hash.digest('hex'), compressedHash.digest('hex'));
+    t.done();
+  });
+
+  input.pipe(bz2C).pipe(writable);
+}
+
 module.exports = {
-  // compressSmallData: t => bz2Codec(4096, t),
-  // compressMediumData: t => bz2Codec(1024 * 1024, t),
-  // compressLargeData: t => bz2Codec(10* 1024 * 1024, t),
-  compressFile: t => {
-    var input = fs.createReadStream('test/raw');
-    var bz2C = bz2.createCompressStream(debug);
-    const hash = crypto.createHash('md5');
-    const compressedHash = crypto.createHash('md5');
-		compressedHash.update(fs.readFileSync('test/compressed'), 'binary');
-
-    var writable = new stream.Writable({
-      write: (chunk, encoding, next) => {
-        hash.update(chunk, 'binary');
-        next()
-      }
-    });
-
-    writable.on('finish', () => {
-      t.eq(hash.digest('hex'), compressedHash.digest('hex'));
-      t.done();
-    });
-
-    input.pipe(bz2C).pipe(writable);
-    // var output = fs.createWriteStream('test/compressed');
-    // output.on('close', () => setTimeout(() => t.done(), 5000));
-    // input.pipe(bz2C).pipe(output);
-  }
+  compressSmallData: t => bz2Codec(4096, t),
+  compressMediumData: t => bz2Codec(1024 * 1024, t),
+  compressLargeData: t => bz2Codec(10* 1024 * 1024, t),
+  compressFile: t => fileCodec('test/raw', 'test/compressed',
+  	bz2.createCompressStream(debug), t),
+  decompressFile: t => fileCodec('test/compressed', 'test/raw',
+  	bz2.createDecompressStream(debug), t),
 };
