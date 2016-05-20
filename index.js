86e4e3faf077f9e1111a3566a7a0f12da76d8873
Kitt Hsu <kitt.hsu@gmail.com>
2016-05-20 14:46:46 +0800

    verbose output

--- index.js
+++ index.js
@@ -7,11 +7,20 @@ var Stream = require('stream');
 const DEFAULT_OUTPUT_BUFFER_SIZE = 8192;
 
 class Bz2Stream extends Stream.Transform {
-  constructor(onCreateStream, onTransform, onFlush) {
+  constructor(onCreateStream, onTransform, onFlush, outputBufSize, debug) {
     super();
     this.bz2Stream = onCreateStream();
     this.onTransform = onTransform;
     this.onFlush = onFlush;
+    this.outputBufSize = outputBufSize;
+    if (debug) {
+      console.log('Enable verbose ' + debug);
+      this.trace = log => {
+        console.log(debug + JSON.stringify(log));
+      };
+    } else {
+      this.trace = () => {};
+    }
   }
 
   _transform(chunk, encoding, callback) {
@@ -23,48 +32,59 @@ class Bz2Stream extends Stream.Transform {
       this.readBuf = chunk;
     }
 
-    const outputBufSize = 8192;
+    this.trace('+ Bytes of input chunk is ' + chunk.length);
+    this.trace('+ Bytes of whole input buffer is ' + this.readBuf.length);
+
     var result = {
       in: this.readBuf.length,
-      out: outputBufSize
+      out: this.outputBufSize
     };
 
     while (result && !result.reachEnd && result.in > 0 && result.out > 0) {
-      var output = Buffer.alloc(outputBufSize);
+      var output = Buffer.alloc(this.outputBufSize);
       result = this.onTransform(this.bz2Stream, this.readBuf, output);
       this.readBuf = this.readBuf.slice(this.readBuf.length - result.in);
+      this.trace('++ Bytes available in input buffer is ' + this.readBuf.length);
+      this.trace('++ Bytes in output buffer is ' + result.out);
+      this.trace('++ Is stream reaches end ? ' + !!result.reachEnd);
       if (result.out > 0) {
+        this.trace('++ Try to push ' + result.out + ' bytes');
         if (!this.push(output.slice(0, result.out))) {
+          this.trace('++ Output queue is full, cant push anymore');
           break;
         }
       }
     }
 
+    this.trace('+ Transform Done');
     callback();
   }
 
   _flush(callback) {
+    this.trace('- Flushing');
     this.onFlush(this.bz2Stream, buf => this.push(buf), callback);
   }
 }
 
-function createDecompressStream() {
+function createDecompressStream(debug, outputBufSize) {
   return new Bz2Stream(
     () => bzip2.decompressInit(),
     (stream, input, output) => bzip2.decompress(stream, input, output),
     (stream, pushCB, cb) => {
       bzip2.decompressEnd(stream);
       cb();
-    }
+    },
+    outputBufSize || DEFAULT_OUTPUT_BUFFER_SIZE,
+    debug ? '[DEC]' : debug
   );
 }
 
-function createCompressStream() {
+function createCompressStream(debug, outputBufSize) {
+  outputBufSize = outputBufSize || DEFAULT_OUTPUT_BUFFER_SIZE;
   return new Bz2Stream(
     () => bzip2.compressInit(),
     (stream, input, output) => bzip2.compress(stream, input, output),
     (stream, pushCB, cb) => {
-      const outputBufSize = 8192;
       var result = {
         out: outputBufSize
       };
@@ -80,107 +100,109 @@ function createCompressStream() {
       }
 
       cb();
-    }
+    },
+    outputBufSize,
+    debug ? '[CO]' : debug
   );
 }
 
-class Bz2FileReadStream extends Stream.Readable {
-  constructor(path) {
-    super();
-    this.path = path;
-    this._openBzip();
-    this.reachEnd = false;
-  }
+// class Bz2FileReadStream extends Stream.Readable {
+//   constructor(path) {
+//     super();
+//     this.path = path;
+//     this._openBzip();
+//     this.reachEnd = false;
+//   }
 
-  _openBzip() {
-    this.bz2Stream = bzip2.decompressInit();
-    this.fd = fs.openSync(this.path, 'r');
-    if (this.fd < 0) throw new Error('Cant open file ' + path);
-  }
+//   _openBzip() {
+//     this.bz2Stream = bzip2.decompressInit();
+//     this.fd = fs.openSync(this.path, 'r');
+//     if (this.fd < 0) throw new Error('Cant open file ' + path);
+//   }
 
-  _closeBzip() {
-    bzip2.decompressEnd(this.bz2Stream);
-    fs.closeSync(this.fd);
-    this.bz2Stream = null;
-    this.fd = null;
-  }
+//   _closeBzip() {
+//     bzip2.decompressEnd(this.bz2Stream);
+//     fs.closeSync(this.fd);
+//     this.bz2Stream = null;
+//     this.fd = null;
+//   }
 
-  _read(size) {
-    if (this.bz2Stream === null || this.fd === null) {
-      this.push(null);
-      return;
-    }
+//   _read(size) {
+//     if (this.bz2Stream === null || this.fd === null) {
+//       this.push(null);
+//       return;
+//     }
 
-    if (!this.reachEnd) {
-      var readBuf = Buffer.alloc(size);
-      var bytesRead = fs.readSync(this.fd, readBuf, 0, size);
-      if (bytesRead === 0) {
-        this.reachEnd = true;
-      } else {
-        if (this.readBuf) {
-          this.readBuf = Buffer.concat(
-            [this.readBuf, readBuf.slice(0, bytesRead)]
-          );
-        } else {
-          this.readBuf = readBuf.slice(0, bytesRead);
-        }
-      }
-    }
+//     if (!this.reachEnd) {
+//       var readBuf = Buffer.alloc(size);
+//       var bytesRead = fs.readSync(this.fd, readBuf, 0, size);
+//       if (bytesRead === 0) {
+//         this.reachEnd = true;
+//       } else {
+//         if (this.readBuf) {
+//           this.readBuf = Buffer.concat(
+//             [this.readBuf, readBuf.slice(0, bytesRead)]
+//           );
+//         } else {
+//           this.readBuf = readBuf.slice(0, bytesRead);
+//         }
+//       }
+//     }
 
-    var outputBuf = Buffer.alloc(size);
-    var result = bzip2.decompress(this.bz2Stream, this.readBuf, outputBuf);
-    if (result.in === 0 && this.reachEnd) this._closeBzip();
+//     var outputBuf = Buffer.alloc(size);
+//     var result = bzip2.decompress(this.bz2Stream, this.readBuf, outputBuf);
+//     if (result.in === 0 && this.reachEnd) this._closeBzip();
 
-    this.readBuf = this.readBuf.slice(this.readBuf.length - result.in);
-    this.push(outputBuf.slice(0, result.out))
-  }
+//     this.readBuf = this.readBuf.slice(this.readBuf.length - result.in);
+//     this.push(outputBuf.slice(0, result.out))
+//   }
 
-  resume() {
-    this._openBzip();
-    super.resume();
-  }
-}
+//   resume() {
+//     this._openBzip();
+//     super.resume();
+//   }
+// }
 
-class Bz2FileWriteStream extends Stream.Writable {
-  constructor(path) {
-    super();
-    this.bz2Stream = bzip2.compressInit();
-    this.fd = fs.openSync(path, 'w');
-    if (this.fd < 0) throw new Error('Cant open file ' + path);
-  }
+// class Bz2FileWriteStream extends Stream.Writable {
+//   constructor(path) {
+//     super();
+//     this.bz2Stream = bzip2.compressInit();
+//     this.fd = fs.openSync(path, 'w');
+//     if (this.fd < 0) throw new Error('Cant open file ' + path);
+//   }
 
-  _writeCB(buf) {
-    var result = fs.writeSync(this.fd, buf, 0, buf.length);
-    if (result !== buf.length) throw new Error('Fail to write file ' + path +
-      ' cause ' + result + ' vs. ' + buf.length);
-  }
+//   _writeCB(buf) {
+//     var result = fs.writeSync(this.fd, buf, 0, buf.length);
+//     if (result !== buf.length) throw new Error('Fail to write file ' + path +
+//       ' cause ' + result + ' vs. ' + buf.length);
+//   }
 
-  _write(chunk, encoding, callback) {
-    var bufs = bzip2.compress(this.bz2Stream, chunk);
-    bufs.map(buf => this._writeCB(buf));
-    callback();
-  }
+//   _write(chunk, encoding, callback) {
+//     var bufs = bzip2.compress(this.bz2Stream, chunk);
+//     bufs.map(buf => this._writeCB(buf));
+//     callback();
+//   }
 
-  _writev(chunks, callback) {
-    chunks.map(chunk =>
-      bzip2.compress(this.bz2Stream, chunk).map(buf => this._writeCB(buf))
-    );
-    callback();
-  }
+//   _writev(chunks, callback) {
+//     chunks.map(chunk =>
+//       bzip2.compress(this.bz2Stream, chunk).map(buf => this._writeCB(buf))
+//     );
+//     callback();
+//   }
 
-  end(chunk, encoding, callback) {
-    super.end(chunk, encoding, err => {
-      if (err) {
-        if (callback) callback(err);
-        return;
-      }
+//   end(chunk, encoding, callback) {
+//     super.end(chunk, encoding, err => {
+//       if (err) {
+//         if (callback) callback(err);
+//         return;
+//       }
 
-      bzip2.compressEnd(this.bz2Stream).map(buf => this._writeCB(buf));
-      fs.closeSync(this.fd);
-      if (callback) callback(err);
-    });
-  }
-}
+//       bzip2.compressEnd(this.bz2Stream).map(buf => this._writeCB(buf));
+//       fs.closeSync(this.fd);
+//       if (callback) callback(err);
+//     });
+//   }
+// }
 
 // function createCompressStream(path) {
 //   return new Bz2FileWriteStream(path);
--- test/index.js
+++ test/index.js
@@ -7,10 +7,10 @@
 
 const bz2 = require('..');
 const stream = require('stream');
-const crypto = require('crypto');
+const debug = false;
 
 function bz2Codec(dataSize, t) {
-  const data = crypto.randomBytes(dataSize);
+  const data = Buffer.alloc(dataSize);
   var output;
   var writable = new stream.Writable({
     write: function(chunk, encoding, next) {
@@ -23,8 +23,8 @@ function bz2Codec(dataSize, t) {
   	t.eq(output.compare(data), 0);
     t.done();
   });
-  var bz2C = new bz2.createCompressStream();
-  var bz2D = new bz2.createDecompressStream();
+  var bz2C = new bz2.createCompressStream(debug);
+  var bz2D = new bz2.createDecompressStream(debug);
   bz2C.pipe(bz2D).pipe(writable);
   bz2C.end(data);
 }
@@ -32,5 +32,5 @@ function bz2Codec(dataSize, t) {
 module.exports = {
   compressSmallData: t => bz2Codec(4096, t),
   compressMediumData: t => bz2Codec(1024 * 1024, t),
-  // compressLargeData: t => bz2Codec(1024* 1024 * 1024, t),
+  compressLargeData: t => bz2Codec(10* 1024 * 1024, t),
 };
